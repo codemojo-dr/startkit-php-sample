688e9a26b699c0ca91cbb74c47b17383a687c954
Shoaib Mohammed <shoaib@ap1.fstpl.com>
2016-03-31 16:52:13 +0530

    Bug fixes

--- sample/includes/codemojo.php
+++ sample/includes/codemojo.php
@@ -14,6 +14,8 @@
 use CodeMojo\Client\Endpoints;
 use CodeMojo\Client\Services\AuthenticationService;
 use CodeMojo\Client\Services\LoyaltyService;
+use CodeMojo\Client\Services\ReferralService;
+use CodeMojo\Client\Services\WalletService;
 
 require_once __DIR__ . '/../config/codemojo.php';
 require_once 'helper.php';
@@ -30,6 +32,10 @@ $authenticationService = new AuthenticationService(CLIENT_ID, CLIENT_SECRET, End
 });
 
 $loyaltyService = new LoyaltyService($authenticationService);
+
+$walletService = new WalletService($authenticationService);
+
+$referralService = new ReferralService($authenticationService);
 
 
 
